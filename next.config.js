142a802f61b98f48e32418669ae5db894a3192cd
Fuzail Abdurakhimov <f.abdurakhimov@jowi.app>
2024-11-23 15:15:37 +0500

    feat: local-project-startup

--- next.config.js
+++ next.config.js
@@ -32,6 +32,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'assets.studiofreight.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
     ],
     formats: ['image/avif', 'image/webp'],
   },
